097e1408dad95ede2e7020ea4308ea18b60bc6bd
SusiSusi <suvi.sipila@helsinki.fi>
2016-09-19 16:18:57 +0300

    postauksen listaussivut ovat valmiit

--- client/modules/Post/components/PostListItem/PostListItem.js
+++ client/modules/Post/components/PostListItem/PostListItem.js
@@ -1,21 +1,36 @@
 import React, { PropTypes } from 'react';
 import { FormattedMessage } from 'react-intl';
 import Time from 'react-time'
+import {Grid, Row, Col, Panel, Pagination,Button, Well, Label, Input, ButtonInput, MenuItem} from 'react-bootstrap';
 
 // Import Style
 import styles from './PostListItem.css';
 
 function PostListItem(props) {
-  var format = "YYYY-MMM-DD DDD";
    return (
-    <div className={styles['single-post']}>
-      <p className={styles['author-name']}><FormattedMessage id="by" /> {props.post.name}</p>
-      <p className={styles['post-desc']}>{props.post.content}</p>
-      <p className={styles['post-desc']}><Time value={props.post.dateAdded} format="DD.MM.YYYY HH:mm:ss"/></p>
-      <p className={styles['post-action']}><a href="#" onClick={props.onDelete}><FormattedMessage id="deletePost" /></a></p>
-      <hr className={styles.divider} />
-    </div>
-  );
+
+     <Grid>
+       <Row className="show-grid">
+         <Col xs={12} md={7}>
+
+           <Panel header={props.post.name}>
+             <p className={styles['post-desc']}>{props.post.content}</p>
+             <p className={styles['post-desc']}><Time value={props.post.dateAdded} format="DD.MM.YYYY HH:mm:ss"/></p>
+             <p className={styles['post-action']}><a href="/" onClick={props.onDelete}><FormattedMessage id="deletePost" /></a></p>
+           </Panel>
+
+         </Col>
+         <Col xs={16} md={4}>
+           <Panel header={props.post.name}>
+             <p className={styles['post-desc']}>{props.post.content}</p>
+             <p className={styles['post-desc']}><Time value={props.post.dateAdded} format="DD.MM.YYYY HH:mm:ss"/></p>
+             <p className={styles['post-action']}><a href="/" onClick={props.onDelete}><FormattedMessage id="deletePost" /></a></p>
+           </Panel>
+         </Col>
+       </Row>
+     </Grid>
+   );
+
 }
 
 PostListItem.propTypes = {
