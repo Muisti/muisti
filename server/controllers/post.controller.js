13c6b1483d1888eef7fdd9ca4495c389e3414275
Jakousa <jakousa@cs.helsinki.fi>
2016-09-19 11:00:27 +0300

    Tyylijuttuja

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,6 +1,5 @@
 import Post from '../models/post';
 import cuid from 'cuid';
-import slug from 'limax';
 import sanitizeHtml from 'sanitize-html';
 
 /**
@@ -70,7 +69,6 @@ export function deletePost(req, res) {
     if (err) {
       res.status(500).send(err);
     }
-
     post.remove(() => {
       res.status(200).end();
     });
