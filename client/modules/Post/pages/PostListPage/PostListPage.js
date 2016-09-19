e84090c943acd992485356a99dd2a762fa07b71a
makesmi <makesmi@hotmail.com>
2016-09-19 18:19:47 +0300

    Postauksen muokkaus (#11)
    * EDIT_POST-action lisõtty ja kõsittely reduceriin
    
    * peruuta-nappi lisõtty
    
    * postauksen muokkaus toimii

--- client/modules/Post/pages/PostListPage/PostListPage.js
+++ client/modules/Post/pages/PostListPage/PostListPage.js
@@ -6,7 +6,7 @@ import PostList from '../../components/PostList';
 import PostCreateWidget from '../../components/PostCreateWidget/PostCreateWidget';
 
 // Import Actions
-import { addPostRequest, fetchPosts, deletePostRequest } from '../../PostActions';
+import { addPostRequest, fetchPosts, deletePostRequest, editPostRequest } from '../../PostActions';
 import { toggleAddPost } from '../../../App/AppActions';
 
 // Import Selectors
@@ -18,6 +18,8 @@ class PostListPage extends Component {
     this.props.dispatch(fetchPosts());
   }
 
+  editingPost = null;
+
   handleDeletePost = post => {
     if (confirm('Do you want to delete this post')) { // eslint-disable-line
       this.props.dispatch(deletePostRequest(post));
@@ -28,12 +30,29 @@ class PostListPage extends Component {
     this.props.dispatch(toggleAddPost());
     this.props.dispatch(addPostRequest({ name, content }));
   };
+  
+  handleEditPost = post => {
+      this.props.dispatch(toggleAddPost());
+      this.props.dispatch(editPostRequest(post));
+  };
+  
+  handleHidePost = () => {
+      this.props.dispatch(toggleAddPost());
+  };
+
+  openEditPost = post => {
+      this.editingPost = post;
+      this.props.dispatch(toggleAddPost());  
+  };
 
   render() {
     return (
       <div>
-        <PostCreateWidget addPost={this.handleAddPost} showAddPost={this.props.showAddPost} />
-        <PostList handleDeletePost={this.handleDeletePost} posts={this.props.posts} />
+        <PostCreateWidget addPost={this.handleAddPost} showAddPost={this.props.showAddPost} 
+            hideAddPost={this.handleHidePost} editPost={this.handleEditPost}
+            originalPost={this.editingPost}/>
+        <PostList handleDeletePost={this.handleDeletePost} posts={this.props.posts}
+            handleEditPost={this.openEditPost}/>
       </div>
     );
   }
