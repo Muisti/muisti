430807164cc912877e82bdc210bca4ef90d7db11
makesmi <makesmi@hotmail.com>
2016-09-22 14:30:46 +0300

    Postauksen muokkaus2 (#13)
    * lisõys toimimaan muokkauksen jõlkeen
    
    * peruuta-nappi korjattu
    
    * Jamin koodista tulleet Warningit korjattu
    
    * listõtty võli lõhetõ ja peruuta-nappien võlille
    
    * submit-buttonille lisõõ ja tallenna -tekstit erikseen
    
    * eri otsikot lisõykselle ja muokkaukselle, submit korjattu
    
    * tõrkeõ-checkbox piilotetaan muokkaustilassa
    
    * lisaa viesti -painike piilotetaan, kun createwidget on nakyvissa
    
    * PostCreateWidget refaktoroitu
    
    * muokkaus-painike toimimaan myos silloin, kun toisen postin muokkaus on kesken
    
    * refaktoroinnin virheet korjattu - muokkaus toimii taas
    
    * postin palautus muokkauksessa lisatty serverin puolelle, jotta muokkauksen jalkeinen paivitys toimii clienstissa
    
    * peruutus tyhjentamaan kentat
    
    * kenttien tyhjentaminen refaktoroitu omaan funktioon
    
    * Muokkaa postia -napin teksti eri kielilla

--- client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
+++ client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
@@ -9,64 +9,95 @@ export class PostCreateWidget extends Component {
 
   constructor(){
       super();
-      this.state = {name: "", content: ""};
+      this.clearFields();
   } 
   
-  originalPost = null;
+  //user is writing new post if originalPost is not defined
+  originalPost = null;  
+  isNewPost = () => !this.originalPost;
   
   // these functions allow input-fields editing
   changeName = event => this.setState({...this.state, name: event.target.value});
   changeContent = event => this.setState({...this.state, content: event.target.value});
   
+  clearFields = () => { this.state = {name: "", content: ""}; }
+  
+  editPost = () => {
+    this.props.editPost({
+        ...this.originalPost,
+        content: this.refs.content.value
+    });
+  };
 
   addPost = () => {
-    if(this.state.originalPost){  //editing existing post
-        console.log("EDITING");
-        this.props.editPost({
-            ...this.state.originalPost,
-            content: this.refs.content.value
-        });
-    }else{  //adding new post
-        console.log("ADDING NEW");
-        const nameRef = this.refs.name;
-        const contentRef = this.refs.content;
-        const importantRef = this.refs.important.checked;
-        if (nameRef.value && contentRef.value) {
-          this.props.addPost(nameRef.value, contentRef.value, importantRef);
-          nameRef.value = contentRef.value = '';
-        }
+    const nameRef = this.refs.name;
+    const contentRef = this.refs.content;
+    const importantRef = this.refs.important.checked;
+    if (nameRef.value && contentRef.value) {
+      this.props.addPost(nameRef.value, contentRef.value, importantRef);
+      nameRef.value = contentRef.value = '';
     }
   };
 
-  render() {
-    const cls = `${styles.form} ${(this.props.showAddPost ? styles.appear : '')}`;
+  submit = () => {
+    if(this.isNewPost()){
+       this.addPost();
+    }else{
+       this.editPost();
+    }
+  };
+  
+  cancel = () => {
+    this.clearFields();
+    this.originalPost = null;
+    this.props.hideAddPost(); 
+  };
+  
+  //updating this components own state based on props
+  updateState = () => {
+    const postChanged = (this.props.originalPost != this.originalPost);
     
-    if(this.props.originalPost != this.originalPost && this.props.originalPost != null){
-        var post = this.props.originalPost;
-        this.state = {name: post.name, content: post.content, originalPost: post};
+    if(postChanged){
+        if(this.props.originalPost == null){
+          this.clearFields();
+        }else{
+          var post = this.props.originalPost;
+          this.state = {name: post.name, content: post.content};
+        }
+    
         this.originalPost = this.props.originalPost;
     }
+  };
+  
+
+  render() {
+    this.updateState();
+          
+    const cls = `${styles.form} ${(this.props.showAddPost ? styles.appear : '')}`;
+    
+    const submitText = this.isNewPost() ? "submitAdd" : "submitEdit";
+    const title = this.isNewPost() ? "createNewPost" : "editPost";
     
     return (
       <div className={cls}> 
         <div className={styles['form-content']}>
-          <h2 className={styles['form-title']}><FormattedMessage id="createNewPost" /></h2>
+          <h2 className={styles['form-title']}><FormattedMessage id={title} /></h2>
   
-          <div class="bootstrap-switch-square">
-              <input type="checkbox" ref="important" /> <FormattedMessage id="isImportant"/>
+          <div className={this.isNewPost() ? 'bootstrap-switch-square' : 'hidden'} > 
+              <input type="checkbox" ref="important"/> <FormattedMessage id="isImportant"/>
           </div>
   
           <input placeholder={this.props.intl.messages.authorName} className={styles['form-field']} ref="name"
             value={this.state.name} onChange={this.changeName}/>
           <textarea placeholder={this.props.intl.messages.postContent} className={styles['form-field']} ref="content" 
             value={this.state.content} onChange={this.changeContent}/>
-          <a className={styles['post-submit-button']} href="#" onClick={this.addPost}><FormattedMessage id="submit" /></a>
-          <a className={styles['post-submit-button']} href="#" onClick={this.hideAddPost}><FormattedMessage id="cancel" /></a>
+          
+            <a className={styles['post-submit-button']} href="#" onClick={this.submit}><FormattedMessage id={submitText} /></a>
+            <a className={styles['post-submit-button']} href="#" onClick={this.cancel}><FormattedMessage id="cancel" /></a>
         </div>
       </div>
     );
   }
-
 }
 
 
