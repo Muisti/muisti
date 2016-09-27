5f937fc7bb7f0ee4d27fe43983860c1aa4fd8f42
Jakousa <jakousa@cs.helsinki.fi>
2016-09-27 13:01:03 +0300

    Siivottiin, headeriin kirjautuminen

--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
-import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Button, Nav, Navbar, NavItem, NavDropdown, MenuItem, FormGroup, FormControl } from 'react-bootstrap';
 import { UserCreateModal } from '../../../User/components/UserCreateModal';
 
 // Import Style
@@ -27,7 +27,19 @@ export function Header(props) {
             <NavItem eventKey={2} href="/signup">Signup</NavItem>
           </Nav>
           <Nav pullRight>
-            <NavItem eventKey={1} href="#">Link Right</NavItem>
+            <NavItem>
+              <Navbar.Form> 
+                <FormGroup>
+                  <FormControl type="text" placeholder="Sähköposti" />
+                </FormGroup>
+                {' '}
+                <FormGroup>
+                  <FormControl type="text" placeholder="Salasana" />
+                </FormGroup>
+                {' '}
+                <Button type="submit">Kirjaudu</Button>
+              </Navbar.Form>
+            </NavItem>
             <NavItem> <UserCreateModal /> </NavItem>
             <NavDropdown eventKey={2} title="Vaihda kieli" id="basic-nav-dropdown">
                 {languageNodes}
