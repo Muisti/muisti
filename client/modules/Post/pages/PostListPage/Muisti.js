ea7534858ba5ecaed427b06c74ecafba87294394
Jakousa <jakousa@cs.helsinki.fi>
2016-10-03 13:14:10 +0300

    Tokeneihin jwt-simple ja kokeilu

--- client/modules/Post/pages/PostListPage/Muisti.js
+++ client/modules/Post/pages/PostListPage/Muisti.js
@@ -1,22 +1,34 @@
-/**
- * Created by susisusi on 12/09/16.
- */
 import React, { Component } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import { UserCreateModal } from '../../../User/components/UserCreateModal';
 import { connect } from 'react-redux';
+import * as jwt from 'jwt-simple';
 
 class Muisti extends Component {
     constructor() {
         super();
     }
 
+    storageTest = () => {
+        if (typeof(Storage) !== "undefined") {
+            // Store
+            var payload = { user: 'Matti' };
+            var secret = 'salasana';
+            var token = jwt.encode(payload, secret);
+            sessionStorage.setItem("user", token);
+            // Retrieve
+            console.log(sessionStorage.getItem("user"));
+        } else {
+            console.log("Sorry, your browser does not support Web Storage...");
+        }
+    }
 
   render() {
     return (
       <div>
         <Button bsStyle="danger"> Testinappi </Button>
         <UserCreateModal />
+        {this.storageTest()}
         <p>Muistiprojekti ....</p>
         
       
