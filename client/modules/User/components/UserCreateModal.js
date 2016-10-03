7e82f95c69bc058ce9c72d3a0955a58b642f1244
SusiSusi <suvi.sipila@helsinki.fi>
2016-10-03 13:19:39 +0300

    user-modal tests

--- client/modules/User/components/UserCreateModal.js
+++ client/modules/User/components/UserCreateModal.js
@@ -10,7 +10,7 @@ export class UserCreateModal extends Component {
       super(props);
       this.state = { showModal: false };
   }
-    
+
   close = () => {
     this.setState({ showModal: false });
   };
@@ -18,7 +18,7 @@ export class UserCreateModal extends Component {
   open = () => {
     this.setState({ showModal: true });
   };
-  
+
   submit = () => {
       this.close();
       const password = this.hash();
@@ -31,20 +31,20 @@ export class UserCreateModal extends Component {
         },
       })
   };
-  
+
   hash = () => {
       var hashed = this.state.formPassword;
       var presalt = (Math.random * (10 + this.state.formEmail.length))+10;
       var salt = bcrypt.genSaltSync(Math.ceil(presalt));
       hashed = bcrypt.hashSync(hashed, salt);
-      
+
       return hashed;
   };
-  
+
   validate = () => {
     var error = '';
     if (!this.validateEmail()) {
-        error = "Sähkopostissasi on kirjoitusvirhe";
+        error = "Sähköpostissasi on kirjoitusvirhe";
     } else if (this.state.formName == '' || this.state.formSurname == '') {
         error = "Onko sinulla etunimi ja sukunimi oikein kirjoitettuna? Tarkista nimesi henkilöllisyystodistuksesta";
     } else if (!this.validatePassword()) {
@@ -54,12 +54,12 @@ export class UserCreateModal extends Component {
     }
     this.setState({ error });
   };
-  
+
   validateEmail = () => {
     const re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     return re.test(this.state.formEmail);
   };
-  
+
   validatePassword = () => {
       var pass = this.state.formPassword;
       var verifier = this.state.formPassVerify;
@@ -70,18 +70,18 @@ export class UserCreateModal extends Component {
       }
       return true;
   };
-  
+
   handleChange = key => e => {
         this.state[key] = e.target.value;
         this.setState({});
   };
-  
+
   registerField = (controlId, label, type, placeholder) => {
     var key = controlId;
     if(this.state[key] === undefined){
         this.state[key] = '';
-    } 
-    return ( 
+    }
+    return (
         <FormGroup controlId={controlId}>
           <Col componentClass={ControlLabel} sm={2}>
             {label}
@@ -102,21 +102,21 @@ export class UserCreateModal extends Component {
         <li onClick={this.open}>
           <FormattedMessage id='displayRegisterModal' />
         </li>
-        
+
         <Modal show={this.state.showModal} onHide={this.close}>
           <Modal.Header closeButton>
             <Modal.Title>Rekisteröidy tästä</Modal.Title>
           </Modal.Header>
-          
+
           <Modal.Body>
           <Form horizontal>
-                
+
                 {this.registerField('formEmail', 'Sähköposti', "email", 'matti.meikalainen@gmail.com')}
                 {this.registerField('formName', 'Etunimi', "text", 'Matti')}
                 {this.registerField('formSurname', 'Sukunimi', "text", 'Meikäläinen')}
                 {this.registerField('formPassword', 'Salasana', "password", 'Salasana')}
                 {this.registerField('formPassVerify', 'Vahvista salasana', "password", 'Salasana')}
-        
+
           </Form>
             <Alert bsStyle="warning" >
                 {this.state.error}
@@ -130,12 +130,12 @@ export class UserCreateModal extends Component {
       </div>
     );
   }
-  
+
 
 }
 
 UserCreateModal.propTypes = {
-      
+
 };
 
 export default UserCreateModal;
