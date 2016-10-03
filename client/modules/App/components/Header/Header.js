ec1a189956393b5f1ab1f089584ca5d4eb756d0c
Jami Kousa <jakousa@cs.helsinki.fi>
2016-10-03 14:35:25 +0300

    Header muokkaus (#24)
    * Tokeneihin jwt-simple ja kokeilu
    
    * Header refaktorointia

--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -12,6 +12,22 @@ export function Header(props) {
   const languageNodes = props.intl.enabledLanguages.map(
       lang => <MenuItem key={lang} onClick={() => props.switchLanguage(lang)} className={lang === props.intl.locale ? styles.selected : ''}>{lang}</MenuItem>
   );
+  
+  const loginBar = (
+        <Navbar.Form pullLeft> 
+                <FormGroup controlId="emailForm" validationState="">
+                  <FormControl type="email" placeholder="Sähköposti" />
+                  <FormControl.Feedback />
+                </FormGroup>
+                {' '}
+                <FormGroup controlId="passwordForm" validationState="">
+                  <FormControl type="password" placeholder="Salasana" />
+                  <FormControl.Feedback />
+                </FormGroup>
+                {' '}
+                <Button type="submit">Kirjaudu</Button>
+        </Navbar.Form>
+    );
 
     return (
       <Navbar>
@@ -27,20 +43,8 @@ export function Header(props) {
             <NavItem eventKey={2} href="/signup">Signup</NavItem>
           </Nav>
             <Nav pullRight>
-              <Navbar.Form pullLeft> 
-                <FormGroup controlId="emailForm" validationState="">
-                  <FormControl type="email" placeholder="Sähköposti" />
-                  <FormControl.Feedback />
-                </FormGroup>
-                {' '}
-                <FormGroup controlId="passwordForm" validationState="">
-                  <FormControl type="password" placeholder="Salasana" />
-                  <FormControl.Feedback />
-                </FormGroup>
-                {' '}
-                <Button type="submit">Kirjaudu</Button>
-              </Navbar.Form>
-                        <NavItem> <UserCreateModal /> </NavItem>
+              {loginBar}
+            <NavItem> <UserCreateModal /> </NavItem>
             <NavDropdown eventKey={2} title="Vaihda kieli" id="basic-nav-dropdown">
                 {languageNodes}
             </NavDropdown>
--- client/modules/Post/pages/PostListPage/Muisti.js
+++ client/modules/Post/pages/PostListPage/Muisti.js
@@ -1,22 +1,36 @@
-/**
- * Created by susisusi on 12/09/16.
- */
 import React, { Component } from 'react';
 import { Button, Modal } from 'react-bootstrap';
 import { UserCreateModal } from '../../../User/components/UserCreateModal';
 import { connect } from 'react-redux';
+import * as jwt from 'jwt-simple';
 
 class Muisti extends Component {
     constructor() {
         super();
     }
 
+    storageOutput = () => {
+        if (typeof(Storage) !== "undefined") {
+            // Store
+            var payload = { user: 'Matti' };
+            var secret = 'salasana';
+            var token = jwt.encode(payload, secret);
+            sessionStorage.setItem("token", token);
+            // Retrieve
+            return (
+                <li> Token: { sessionStorage.getItem("user") } </li>
+            );
+        } else {
+            console.log("Sorry, your browser does not support Web Storage...");
+        }
+    }
 
   render() {
     return (
       <div>
         <Button bsStyle="danger"> Testinappi </Button>
         <UserCreateModal />
+        {this.storageOutput()}
         <p>Muistiprojekti ....</p>
         
       
