14736d5237d127fc99efba4536ccf96245bc6ee7
Markus Mustonen <makesmi@hotmail.com>
2016-10-17 11:59:06 +0300

    testien kokeilu vahvistukselle

--- server/controllers/__tests__/user.controller.spec.js
+++ server/controllers/__tests__/user.controller.spec.js
@@ -3,11 +3,22 @@ import request from 'supertest';
 import app from '../../server';
 import sinon from 'sinon';
 import { connectDB, dropDB } from '../../util/test-helpers';
+import User from '../../models/user';
+
 var proxyquire = require('proxyquire');
 
+const users = [
+  new User({ name: 'Alice', surname: 'Knox', email: 'a@aa.fi', password: 'testing12', cuid: 'f34gb2bh24b24b2' }),
+  new User({ name: 'Ben', surname: 'Test', email: 'n@aa.fi', password: 'testing23', cuid: 'f34gb2bh24b24b3' }),
+  new User({ name: 'Jon', surname: 'Testing', email: 'n@xa.fi', password: 'testing23', cuid: 'f34gb2bh24b24b4' }),
+];
 
 test.beforeEach.serial('connect and add tree users', t => {
-  connectDB(t, () => {});
+  connectDB(t, () => {
+    User.create(users, err => {
+      if (err) t.fail('Unable to create users');
+    });
+  });
 });
 
 test.afterEach.always.serial(t => {
@@ -16,15 +27,8 @@ test.afterEach.always.serial(t => {
   });
 });
 
-test.serial('Adding user sends confirmation email', async t => {
-   t.plan(2);
-
-  var called = false;
-  var stub = function(){
-          called = true;
-
-  };
-  proxyquire('../user.controller', {'nodemailer': stub, 'mailer': stub});
+test.serial('Creating user generates confirmation code', async t => {
+  t.plan(3);
 
   const res = await request(app)
     .post('/api/users')
@@ -32,6 +36,10 @@ test.serial('Adding user sends confirmation email', async t => {
     .set('Accept', 'application/json');
 
   t.is(res.status, 200);
-  t.truthy(called);
-  
+
+  users.push(res);
+
+  const savedUser = await User.findOne({ surname: 'One' }).exec();
+  t.is(savedUser.name, 'New');
+  t.deepEqual(users.length, 4);
 });
