9c8a7ca8858c51b7929d2056c8dddb81ef3705dd
Jakousa <jakousa@cs.helsinki.fi>
2016-10-18 14:57:48 +0300

    testejä lisää

--- client/modules/App/__tests__/Header/LoginBox.spec.js
+++ client/modules/App/__tests__/Header/LoginBox.spec.js
@@ -4,6 +4,8 @@ import sinon from 'sinon';
 import { FormattedMessage } from 'react-intl';
 import { LoginBox } from '../../components/Header/LoginBox';
 import { mountWithIntl, shallowWithIntl } from '../../../../util/react-intl-test-helper';
+import { UserCreateModal } from '../../../User/components/UserCreateModal';
+import * as authStorage from '../../../../util/authStorage';
 
 test('renders properly', t => { 
   const wrapper = shallowWithIntl(
@@ -12,6 +14,22 @@ test('renders properly', t => {
   
   t.is(wrapper.find('FormGroup').length, 2);
   t.is(wrapper.find('Button').length, 1);
+  t.is(wrapper.find('UserCreateModal').length, 1);
+});
+
+test('renders properly when logged in', t => {
+    var stub = sinon.stub(authStorage, 'getToken');
+    stub.returns("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjdWlkIjoiY2l1ZHBtZGo2MDAwMHRha3I0NmVnZmEyNCIsInVzZXIiOiJBbmltaSIsInRpbWUiOjE0NzY3ODUwMjA2NjZ9.Du7CVJgWA5Kju4BjEF1ENhlW5GBCs7r3vtO6oOvHJr0");
+ 
+    const wrapper = shallowWithIntl(
+     <LoginBox />
+    );
+    
+   
+    t.is(wrapper.find('Button').length, 1);
+    t.is(wrapper.find('UserCreateModal').length, 0)
+    
+    stub.restore();
 });
 
 //test('logIn gets called', t => {
@@ -30,25 +48,6 @@ test('renders properly', t => {
 //
 //});
 
-//
-//test('LogIn sets validation states correctly', t => {
-//	const wrapper = shallowWithIntl(
-//	<LoginBox/>
-//	);
-//
-//	var wrap = wrapper.instance();
-//	var spy = sinon.spy(wrap, 'setToken');
-//	wrap.forceUpdate();
-//	wrapper.update();
-//
-//
-//	wrap.setToken();
-//	console.warn(wrap);
-//	t.is(wrap.state.validEmail, "error");
-//
-//	spy.restore();
-//});
-
 test('setToken works correctly', t =>  {
   const wrapper = shallowWithIntl(
     <LoginBox />
@@ -58,12 +57,6 @@ test('setToken works correctly', t =>  {
   instance.checkToken()
 
   t.truthy(instance.state.validPass == "error");
-
-//  console.log(spy.calledWith("token", "token"));
-//  t.truthy(spy.calledWith("token", "token"));
-//  
-  // Reset localStorage.setItem method    
-//  spy.reset();
 });
 
 test('setToken works correctly 2', t =>  {
@@ -77,24 +70,17 @@ test('setToken works correctly 2', t =>  {
   t.truthy(instance.state.validEmail == "error");
 });
   
-//test.only('setToken works correctly 3', t =>  {
-//  const wrapper = mountWithIntl(
-//    <LoginBox fetchPosts={() => {}}/>
-//  );
-//  
-//  console.log("1111111111111");
-//  var instance = wrapper.instance();
-////  Miten ketussa vakoillaan componentin ulkopuolista funktiota
-//  var spy = sinon.spy(setToken);
-//  
-//  instance.checkToken("jotainjotaintokeni")
-//  
-//  console.log("2222222222222");
-//
-//  t.truthy(spy.called);
-//  
-//  console.log("333333333333");
-//  });
+test('setToken works correctly 3', t =>  {
+  const wrapper = shallowWithIntl(
+    <LoginBox fetchPosts={() => {}}/>
+  );
+  var instance = wrapper.instance();
+  var spy = sinon.spy(authStorage, 'setToken');
+  
+  instance.checkToken("jotainjotaintokeni")
+
+  t.truthy(spy.calledOnce);
+  });
 
 test('logIn does not accept empty lines', t => {
     const wrapper = mountWithIntl(
@@ -113,3 +99,16 @@ test('logIn does not accept empty lines', t => {
     
     t.truthy(spy.calledWith("email"));
 });
+
+test('logOut works as intended', t => {
+  const wrapper = shallowWithIntl(
+    <LoginBox fetchPosts={() => {}}/>
+  );
+  var instance = wrapper.instance();
+  var spy = sinon.spy(authStorage, 'removeToken');
+  
+  instance.logOut();
+  
+  t.truthy(spy.calledOnce);
+});
+
