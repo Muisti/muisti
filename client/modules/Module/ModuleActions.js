7be70ea7a86d323d1a6bc8693be0e695a2c40ac5
Jakousa <jakousa@cs.helsinki.fi>
2016-10-31 15:05:03 +0200

    mergettiin bbackend ja front moduulilistaukselle

--- client/modules/Module/ModuleActions.js
+++ client/modules/Module/ModuleActions.js
@@ -1,8 +1,6 @@
 import callApi from '../../util/apiCaller';
 
 export function fetchModules() {
-//    return callApi('modules', 'get')
-//      .then(res => res.user);
-      console.log("fetchModules kutsuttiin");
-      return Promise.resolve([{ title: "11nimi", content: "11sisalto" }, { title: "22nimi", content: "22sisalto" }]);
+    return callApi('modules', 'get')
+      .then(res => res.modules);
 }
