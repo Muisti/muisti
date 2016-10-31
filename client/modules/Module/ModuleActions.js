f7503af90b8a868de33d59722513991ceb376d15
Soininen Simo S <sssoinin@dat-14.cs.helsinki.fi>
2016-10-31 16:56:34 +0200

    modelpagea tekemässä

--- client/modules/Module/ModuleActions.js
+++ client/modules/Module/ModuleActions.js
@@ -4,3 +4,13 @@ export function fetchModules() {
     return callApi('modules', 'get')
       .then(res => res.modules);
 }
+
+export function fetchModule(title){
+	return {module: {cuid: 1111, title: 'eka osa'}}
+
+}
+
+export function fetchSections(cuid){
+	return {sections: [{cuid: 1111, text: 'sisältöä tässä'},{cuid: 1111, text: 'toinen tässä'}]}
+
+}
