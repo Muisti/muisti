188c7c1bad6983124f7df2d9da0b17e8110ea5f9
Jakousa <jakousa@cs.helsinki.fi>
2016-11-01 10:32:39 +0200

    Moduulin lisäys

--- client/modules/Module/ModuleActions.js
+++ client/modules/Module/ModuleActions.js
@@ -4,3 +4,10 @@ export function fetchModules() {
     return callApi('modules', 'get')
       .then(res => res.modules);
 }
+
+export function addModuleRequest(module) {
+    console.log(module);
+    return Promise.resolve({ title: 'Otsikko', info: 'Sisältö' })
+//    return callApi('modules', 'post', {module})
+//            .then(res => res.module);
+}
