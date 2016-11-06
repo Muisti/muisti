35f162c508120ad6acae80ad6aa2b21754374292
Simo Soininen <soinisi@gmail.com>
2016-11-06 22:58:09 +0200

    Lisätty testejä SectionCreateModaliin

--- client/modules/Module/components/ModuleListItem/ModuleListItem.js
+++ client/modules/Module/components/ModuleListItem/ModuleListItem.js
@@ -1,15 +1,13 @@
 import React, { PropTypes } from 'react';
 import { PageHeader, Panel } from 'react-bootstrap';
-
+import ModulePage from '../../pages/ModulePage'
 function ModuleListItem(props) {
   var toAddress = encodeURI(props.module.title);
-  console.log(toAddress);
+  
   return (
     <div>
-
       {props.module.info}
       <a href={"/module/" + toAddress} className="btn btn-default pull-right" >Siirry</a>
-
     </div>
   );
 }
