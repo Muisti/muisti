8886e2a7908584aa18587f0bf353bcc89471379c
Simo Soininen <soinisi@gmail.com>
2016-11-06 22:58:09 +0200

    Lisätty testejä SectionCreateModaliin

---
+++ client/modules/Module/__tests__/components/SectionCreateModal.spec.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import test from 'ava';
+import sinon from 'sinon';
+import { shallow } from 'enzyme';
+import SectionCreateModal from '../../components/SectionCreateModal/SectionCreateModal';
+import { mountWithIntl, shallowWithIntl } from '../../../../util/react-intl-test-helper';
+import * as SectionActions from '../../SectionActions';
+
+
+test('SectionsCreateModal renders properly', t => {
+  const wrapper = shallowWithIntl(
+    <SectionCreateModal />
+  );
+
+  t.is(wrapper.find('span').length, 1);
+  t.is(wrapper.find('form').length, 1);
+  t.is(wrapper.find('Button').length, 3);
+});
+
+test('handleAddSections does not add section if content empty', t =>{
+	const wrapper = shallowWithIntl(
+    <SectionCreateModal />
+  );
+	var instance = wrapper.instance();
+
+	var stub = sinon.stub(SectionActions, 'addSectionRequest');
+	stub.returns(Promise.resolve({}));
+
+	instance.setState({formContent: ""});
+
+	instance.handleAddSection();
+
+	t.truthy(!stub.called);
+	stub.restore();
+
+});
+
+
+test('handleAddSections calls addSectionRequest', t =>{
+	const wrapper = shallowWithIntl(
+    <SectionCreateModal />
+  );
+	var instance = wrapper.instance();
+
+	var stub = sinon.stub(SectionActions, 'addSectionRequest');
+	stub.returns(Promise.resolve({}));
+
+	instance.setState({formContent: "koe!"});
+
+	instance.handleAddSection();
+
+	t.truthy(stub.calledOnce);
+	stub.restore();
+
+});
+
