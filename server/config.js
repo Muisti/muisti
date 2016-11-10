803f8e3b477375161992d54012a85702f877e13e
Jami Kousa <jakousa@cs.helsinki.fi>
2016-11-10 13:26:43 +0200

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
   mongoURL: process.env.NODE_ENV == 'test' ? 
-            'mongodb://väärä:väärä@ds029436.mlab.com:29436/testikanta' : 
+            'mongodb://localhost:27017/muisti' : 
             process.env.MONGO_URL || 'mongodb://ohtumuisti:ohtu2016@ds147487.mlab.com:47487/muistidevaus',
   port: process.env.PORT || 8000,
 };
