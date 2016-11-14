0e54adfcbed1303e5029c60d04885f252c212ddc
SusiSusi <suvi.sipila@helsinki.fi>
2016-11-14 16:00:11 +0200

    config

--- server/config.js
+++ server/config.js
@@ -1,8 +1,8 @@
 const config = {
-  mongoURL: process.env.NODE_ENV == 'test' ? 
-            'mongodb://localhost:27017/muisti' : 
-            process.env.MONGO_URL || 'mongodb://ohtumuisti:ohtu2016@ds147487.mlab.com:47487/muistidevaus',
-  port: process.env.PORT || 8000,
+  mongoURL: process.env.NODE_ENV == 'test' ?
+    'mongodb://testaaja:testaaja@ds147497.mlab.com:47497/testikanta2' :
+  process.env.MONGO_URL || 'mongodb://ohtumuisti:ohtu2016@ds147487.mlab.com:47487/muistidevaus',
+  port: process.env.PORT || 8000
 };
 
 export default config;
