bc1c840979d4c9937c3fb5ea83221e2c000e7371
Markus Mustonen <makesmi@hotmail.com>
2016-11-14 14:52:18 +0200

    kayttajan valintojen haku

--- client/modules/Module/components/Section.js
+++ client/modules/Module/components/Section.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { PageHeader, Panel } from 'react-bootstrap';
+import { PageHeader, Panel, Button } from 'react-bootstrap';
 import { injectIntl, intlShape, FormattedMessage } from 'react-intl';
 import validator from 'validator';
 
@@ -29,25 +29,46 @@ export class Section extends Component{
 
   }
   
-  renderQuiz = quiz => {
+  renderQuiz = (quiz, number) => {
+      var optionNumber = 0;
+      
       return (
-        <div>
+        <div style={{marginBottom: '15px'}}>
         <div style={{color: '#5555bb', fontWeight: 'bold', marginBottom: '10px'}}>
-        <span style={{marginRight: '8px'}}>
-            1.
+        <span style={{marginRight: '9px', background: '#dfdfff', borderRadius: '15px', 
+            padding: '4px', paddingLeft: '9px', paddingRight: '5px', fontSize: '1.15em'}}>
+          {number + '.'}
         </span>
         {quiz.question}</div>
-          {quiz.answers.map(answer => (
+          {quiz.options.map(option => (
             <div style={{marginLeft: '25px'}}>
-            <label><input type="checkbox" style={{marginRight: '6px'}} />{answer}</label>
+            <label><input id={'quiz' + (number-1) + 'option' + optionNumber++}
+                type="checkbox" style={{marginRight: '6px'}} />{option.text}</label>
             </div>
           ))}
         </div>
       );
   }
   
+  getUserSelections = (quiz, index) => {
+    var result = [];
+
+    for(var i = 0; i < quiz.options.length; i++){
+      var checked = document.getElementById('quiz' + index + 'option' + i).checked;
+      result.push(checked);
+    }
+    
+    return result;
+  };
+  
     render(){
       var section = this.props.section;
+      var quizOrderNumber = 0;
+      section.quizzes = [{question: 'mikä eläin sanoo "Hau Hau" ?', options: [
+        {text: 'kissa'}, {text: 'koira'}, {text: 'kukko'}
+      ]}, {question: 'mikä eläin sanoo "Mau Mau" ?', options: [
+        {text: 'kissa'}, {text: 'koira'}, {text: 'kukko'}
+      ]}];
         
       return (
         <Panel collapsible defaultExpanded header={section.title ? section.title : ''} >
@@ -55,20 +76,21 @@ export class Section extends Component{
 
          {section.link ? this.renderMultimediaFileType(this.checkMultimediaFileType(section.link), section) : ''}
          
-         <div show={true}>
-         <br />
-         <Panel collapsible header={(
-             <div className='clearfix'>
-             <div style={{cursor: 'pointer'}}>
-                 <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/Question_Circle.svg?uselang=fi" />
-             <span style={{marginLeft: '7px', color: '#000066'}}>
-                Tehtävät
-             </span>
-             </div>
-             </div>)}>
-         {this.renderQuiz({question: "Mikä eläin sanoo 'hau hau'?", 
-                 answers: ['koira', 'kissa', 'kukko']})}
-         </Panel>
+         <div style={!section.quizzes || section.quizzes.length == 0 ? {display: 'none'} : {}}>
+            <br />
+            <Panel collapsible header={(
+                <div className='clearfix'>
+                <div style={{cursor: 'pointer'}}>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/Question_Circle.svg?uselang=fi" 
+                        style={{ verticalAlign: 'bottom' }}/>
+                <span style={{marginLeft: '7px', color: '#000066'}}>
+                   Tehtävät
+                </span>
+                </div>
+                </div>)}>
+            {section.quizzes.map(quiz => this.renderQuiz(quiz, ++quizOrderNumber))}
+            </Panel>
+            <Button onClick={() => console.log(this.getUserSelections(section.quizzes[0], 0))}>tarkista</Button>
          </div>
         </Panel>
       );  
@@ -81,6 +103,13 @@ Section.propTypes = {
       content: PropTypes.string,
       title: PropTypes.string,
       link: PropTypes.string,
+      quizzes: PropTypes.arrayOf({
+          question: PropTypes.string,
+          options: PropTypes.arrayOf({
+              text: PropTypes.string,
+              answer: PropTypes.bool
+          })
+      }),
   })
 };
 
