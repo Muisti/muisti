ad67ec49e3345747f442befc6ddaf35382ba5143
SusiSusi <suvi.sipila@helsinki.fi>
2016-11-21 12:59:56 +0200

    client section-luokka ja section-luokan testit

--- client/modules/Module/__tests__/components/Section.spec.js
+++ client/modules/Module/__tests__/components/Section.spec.js
@@ -2,40 +2,71 @@ import React from 'react';
 import test from 'ava';
 import sinon from 'sinon';
 import { shallow } from 'enzyme';
-import Section from '../../components/Section';
+import { Section } from '../../components/Section';
 import { mountWithIntl, shallowWithIntl } from '../../../../util/react-intl-test-helper';
 import * as moduleActions from '../../ModuleActions';
 import validator from 'validator';
 
+const props = {
+  section: {
+    cuid: 'cuid12',
+    content: 'Sections content',
+    title: 'Title',
+    quizzes: [{sectionCuid: 'cuid12', question: 'What animal?', options: [{ text: 'Cat', answer: true }, {text: 'Snake', answer: false}]}]
+  }
+};
+
 test('renders properly', t => {
-  
-	const imageSection = { title: 'Section title', content: 'Sections content', link:'https://muistioppi.herokuapp.com/koe.jpg', orderNumber: 1, moduleCuid: 'cuid12', cuid: 'secCuid' };
+  const wrapper = shallowWithIntl(
+    <Section {...props}/>
+  );
+
+  t.is(wrapper.find('Panel').length, 1);
+  t.is(wrapper.find('QuizPanel').length, 0);
+});
+
+test('Renders image-filetype correctly', t => {
+  const imageSection = { content: 'Sections content', link:'http://i.imgur.com/hXvANaX.jpg' };
+
   const wrapper = shallowWithIntl(
     <Section section={imageSection}/>
   );
 
-  t.is(wrapper.find('Panel').length, 0);
+  let instance = wrapper.instance();
+
+  const image = instance.checkMultimediaFileType(imageSection.link);
+  const imageRender = instance.renderMultimediaFileType(image, imageSection);
+
+  t.is(image, "image");
+  t.is(imageRender.type, "img");
 });
 
-test('Renders mediafiletype correctly', t => {
-	const module = { title: 'title', info: 'moduulin info', orderNumber: 1, cuid: 'cuid12' };
-	const imageSection = { title: 'Section title', content: 'Sections content', link:'https://muistioppi.herokuapp.com/koe.jpg', orderNumber: 1, moduleCuid: 'cuid12', cuid: 'secCuid' };
-	const videoSection = { title: 'Section title', content: 'Sections content', link:'https://muistioppi.herokuapp.com/koe.webm', orderNumber: 1, moduleCuid: 'cuid12', cuid: 'secCuid2' };
+test('Renders video-filetype correctly', t => {
+  const videoSection = { content: 'Sections content', link:'http://www.w3schools.com/html/mov_bbb.mp4' };
 
-	const wrapper = shallowWithIntl(
-		<Section />
-	);
+  const wrapper = shallowWithIntl(
+    <Section section={videoSection}/>
+  );
 
-	var instance = wrapper.instance();
-	
-	const image = instance.checkMultimediaFileType(imageSection.link);
-	const video = instance.checkMultimediaFileType(videoSection.link);
-	const imageRender = instance.renderMultimediaFileType(image, imageSection);
-	const videoRender = instance.renderMultimediaFileType(video, videoSection);
+  let instance = wrapper.instance();
 
-	t.is(image, "image");
-	t.is(video, "video");
-	t.is(imageRender.type, "img");
-	t.is(videoRender.type, "video");
+  const video = instance.checkMultimediaFileType(videoSection.link);
+  const videoRender = instance.renderMultimediaFileType(video, videoSection);
 
+  t.is(video, "video");
+  t.is(videoRender.type, "video");
+});
+
+test('Add quizzes correctly', t => {
+  const wrapper = shallowWithIntl(
+    <Section {...props} />
+  );
+
+  const quiz = { sectionCuid: 'cuid142', question: 'What animal?', options: [{ text: 'Lion', answer: true }, {text: 'Cow', answer: false}] };
+
+  let instance = wrapper.instance();
+  t.is(instance.props.section.quizzes.length, 1);
+
+  instance.addQuiz(quiz);
+  t.is(instance.props.section.quizzes.length, 2);
 });
