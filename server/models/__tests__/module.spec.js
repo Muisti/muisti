b66660d32aea38631422a3e08d8ebe9db790470a
Simo Soininen <soinisi@gmail.com>
2016-11-28 02:01:52 +0200

    Testiyrityksiä modulemodelille

--- server/models/__tests__/module.spec.js
+++ server/models/__tests__/module.spec.js
@@ -2,37 +2,31 @@ import test from 'ava';
 import request from 'supertest';
 import app from '../../server';
 import sinon from 'sinon';
-import User from '../user';
+import Module from '../module';
+import Section from '../section'
 import { connectDB, dropDB } from '../../util/test-helpers';
 
-const module = new Module({cuid: '583b0e90b9492c16c4acc462', title: 'Moduuli', info:'sis', orderNumber: 1});
-const sections = [new Section({
-    cuid: 'cuid12',
-    content: 'Sections content',
-    title: 'Title',
-    quizzes: [{sectionCuid: 'cuid12', question: 'What animal?', options: [{ text: 'Cat', answer: true }, {text: 'Snake', answer: false}]}]
-  }),new Section({
-    cuid: 'cuid13',
-    content: 'Sections content',
-    title: 'Title',
-    quizzes: [{sectionCuid: 'cuid13', question: 'What animal?', options: [{ text: 'Cat', answer: true }, {text: 'Snake', answer: false}]}]
-  })
-];
+const newModule = new Module({cuid: '583b0e90b9492c16c4acc462', title: 'Moduuli', info:'sis', orderNumber: 1 });
+const newSection = new Section({cuid:'2345677888888989', moduleCuid:'583b0e90b9492c16c4acc462', title: 'koe title', orderNumber: 1})
+const newSection2 = new Section({cuid:'2222222222222222', moduleCuid:'583b0e90b9492c16c4acc462', title: 'koe title 2', orderNumber: 2})
 
-test.beforeEach.serial('connect and add one module two sections', t => {
-  connectDB(t, () => {
-    Module.create(module, err => {
-      if (err) t.fail('Unable to create module');
-    });
- 	 	 Module.create(sections, err => {
-      if (err) t.fail('Unable to create sections');
-    });
-	});
 
-});
 
-test.afterEach.always.serial(t => {
-  dropDB(t, () => {
-    return;
-  });
+test.serial('Removes sections when module removed', async t => {
+	await newModule.save();
+	await newSection.save();
+	
+
+	await newModule.remove();
+	var section = await Section.find().exec();
+	//const module = await Module.find().exec((err, m) => {console.log(m)});
+	
+	console.log(section);
+
+	t.is(section.length, 0);
+	
+
+
+
+
 });
