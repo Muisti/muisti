4af310b1543f18ae73f4fbfc6d416a5f92691e3c
Simo Soininen <soinisi@gmail.com>
2016-12-04 21:40:43 +0200

    rikki menneet testit korjattu

--- client/modules/User/__tests__/components/UserCreateModal.spec.js
+++ client/modules/User/__tests__/components/UserCreateModal.spec.js
@@ -4,46 +4,50 @@ import sinon from 'sinon';
 import { FormattedMessage } from 'react-intl';
 import { UserCreateModal } from '../../components/UserCreateModal';
 import { mountWithIntl, shallowWithIntl } from '../../../../util/react-intl-test-helper';
+import * as userActions from '../../UserActions'
+import* as authStorage from '../../../../util/authStorage'
+
 
 test('renders properly', t => {
   const wrapper = shallowWithIntl(
-    <UserCreateModal />
+    <UserCreateModal editUser={false}/>
   );
 
   t.is(wrapper.find('span').length, 1);
   t.is(wrapper.find('FormGroup').length, 5);
-  t.is(wrapper.find('Button').length, 4);
+  t.is(wrapper.find('Button').length, 3);
 
 });
 
 test('returns errors correctly', t => {
   const wrapper = shallowWithIntl(
-    <UserCreateModal />
+    <UserCreateModal editUser={false}/>
   );
 
-  wrapper.find('Button').at(1).simulate('click');
+  wrapper.find('Form').simulate('submit');
   var emailError = wrapper.state().error;
+ 
   wrapper.setState({ formEmail: 'sahko@posti.com' })
 
-  wrapper.find('Button').at(1).simulate('click');
+  wrapper.find('Form').simulate('submit');
   var nameError = wrapper.state().error;
   wrapper.setState({ formName: 'Nimi' })
   wrapper.setState({ formSurname: 'Sukunimi' })
 
-  wrapper.find('Button').at(1).simulate('click');
+  wrapper.find('Form').simulate('submit');
   var passError = wrapper.state().error;
   wrapper.setState({ formPassword: 'salasana123' });
   wrapper.setState({ formPassVerify: 'salasana123' });
 
-  wrapper.find('Button').at(1).simulate('click');
-
+  wrapper.find('Form').simulate('submit');
+  
   t.truthy(emailError != nameError != passError);
   t.truthy(wrapper.state().error == '')
 });
 
 test('return correct colors', t => {
   const wrapper = shallowWithIntl(
-    <UserCreateModal />
+    <UserCreateModal editUser={false}/>
   );
 
   let instance = wrapper.instance();
@@ -64,4 +68,3 @@ test('return correct colors', t => {
   t.is(colorPassword, null);
   t.is(colorPassVerify, 'warning');
 });
-
