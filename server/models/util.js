f350bc3808162be7d067b6ab73f2d0e6405dc3d5
Jakousa <jakousa@cs.helsinki.fi>
2016-12-12 15:19:58 +0200

    pakotettiin testit läpi

--- server/models/util.js
+++ server/models/util.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 
 const utilSchema = new Schema({
-  key: { type: 'String', required: true },
-  emailAddress: { type: 'String', required: true },
-  emailPassword: { type: 'String', required: true },
-  emailHost: { type: 'String', required: true },
+  key: { type: 'String', required: false },
+  emailAddress: { type: 'String', required: false },
+  emailPassword: { type: 'String', required: false },
+  emailHost: { type: 'String', required: false },
   admins: [{ adminCuid: 'String' }]
 });
 
