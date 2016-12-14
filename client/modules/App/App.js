5c23a85a99a9149e24882ef323852bf08c98646b
Simo Soininen <soinisi@gmail.com>
2016-12-14 20:22:55 +0200

    heroku7

--- client/modules/App/App.js
+++ client/modules/App/App.js
@@ -28,7 +28,7 @@ export class App extends Component {
       <div>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
         integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
-        crossorigin="anonymous"></link>
+        crossOrigin="anonymous"></link>
 
         <PassStorage />
         <div>
