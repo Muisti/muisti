7782d7f2aff942e2c44f5c3e10750e17c6a90e93
Simo Soininen <soinisi@gmail.com>
2016-12-14 19:20:37 +0200

    heroku3

--- client/modules/App/App.js
+++ client/modules/App/App.js
@@ -26,7 +26,6 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"></link>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"></link>
 
         <PassStorage />
